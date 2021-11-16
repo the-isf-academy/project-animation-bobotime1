73fb5d4c734775d98f7723f6dadb9c7b639ff3c4
Angus <0113002@student.isf.edu.hk>
2021-11-16 11:53:51 +0800

    What did you work on in this commit? Started my animation project, and attempted to draw an apple in object.py

---
+++ object.py
@@ -0,0 +1,44 @@
+from turtle import *
+
+def circle():
+    for i in range(360):
+        forward(5)
+        right(1000/360)
+
+
+
+
+def apple():
+    forward(10)
+    right(5)
+    forward(1)
+    right(5)
+    forward(10)
+    right(5)
+    forward(10)
+    right(15)
+    forward(5)
+    right(15)
+    forward(5)
+    right(15)
+    forward(5)
+    right(30)
+    forward(5)
+    right(5)
+    forward(15)
+    right(5)
+    forward(15)
+    right(10)
+    forward(15)
+    right(15)
+    forward(5)
+    right(45)
+    forward(10)
+    right(5)
+    forward(20)
+
+
+
+circle()
+
+input()
