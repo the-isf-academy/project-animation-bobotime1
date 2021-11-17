35e1f6dc3a23fbec9140fc9fb9bcaaf60f97c2ec
Angus <0113002@student.isf.edu.hk>
2021-11-17 11:52:31 +0800

    What did you work on in this commit? I finished drawing an apple with a for loop that can also change the size factor

--- object.py
+++ object.py
@@ -1,44 +1,80 @@
 from turtle import *
 
-def circle():
-    for i in range(360):
-        forward(5)
-        right(1000/360)
+def apple(size):
+
+    for i in range(9):
+            if i == 0:
+                forward(size/5)
+                right(10)
+                forward(size/5)
+                right(5)
+                forward(size/5)
+                right(15)
+            if i == 1:
+                forward(size/10)
+                right(15)
+                forward(size/10)
+                right(15)
+                forward(size/10)
+                right(30)
+                forward(size/10)
+                right(5)
+            if i == 2:
+                forward(size/(10/3))
+                right(5)
+                forward(size/(10/3))
+                right(10)
+                forward(size/(10/3))
+                right(15)
+            if i == 3:
+                forward(size/10)
+                right(45)
+                forward(size/5)
+                right(5)
+            if i == 4:
+                forward(size/5)
+                right(5)
+                forward(size/10)
+                right(10)
+                forward(size/5)
+                right(10)
+                forward(5)
+                left(10)
+                forward(5)
+                right(5)
+            if i == 5:
+                forward(10)
+                right(45)
+                forward(5)
+                right(15)
+            if i == 6:
+                forward(15)
+                right(10)
+                forward(15)
+                right(5)
+                forward(15)
+                right(5)
+            if i == 7:
+                forward(5)
+                right(30)
+                forward(5)
+                right(15)
+                forward(5)
+                right(15)
+                forward(5)
+                right(15)
+            if i == 8:
+                forward(10)
+                right(11)
+                forward(9)
+                left(85)
+                forward(20)
 
 
 
-
-def apple():
-    forward(10)
-    right(5)
-    forward(1)
-    right(5)
-    forward(10)
-    right(5)
-    forward(10)
-    right(15)
-    forward(5)
-    right(15)
-    forward(5)
-    right(15)
-    forward(5)
-    right(30)
-    forward(5)
-    right(5)
-    forward(15)
-    right(5)
-    forward(15)
-    right(10)
-    forward(15)
-    right(15)
-    forward(5)
-    right(45)
-    forward(10)
-    right(5)
-    forward(20)
+apple(50)
 
 
 
-circle()
 
 input()
